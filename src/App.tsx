e4afc95d06f2987d65e7630c342a788aadf7e3a9
giovanni.maffeo <giovanni.maffeo@gmail.com>
2024-11-05 12:28:28 +0000

    feat: color palette

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ const DrawingCanvas = () => {
   const canvasRef = useRef(null);
   const contextRef = useRef(null);
   const [isDrawing, setIsDrawing] = useState(false);
+  const [color, setColor] = useState("#000000")
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -45,15 +46,33 @@ const DrawingCanvas = () => {
     contextRef.current.stroke();
   };
 
+  const handleColorChange = (event) => {
+    console.log(event.target.value)
+    setColor(event.target.value);
+  }
+
+  useEffect(() => {
+    if (contextRef.current) {
+      contextRef.current.strokeStyle = color;
+    }
+  }, [color]);
+
   return (
-    <canvas
-      ref={canvasRef}
-      onMouseDown={startDrawing}
-      onMouseUp={finishDrawing}
-      onMouseMove={draw}
-      onMouseLeave={finishDrawing} // Para parar de desenhar se o mouse sair do canvas
-      style={{ border: "1px solid black" }}
-    />
+    <div>
+      <canvas
+        ref={canvasRef}
+        onMouseDown={startDrawing}
+        onMouseUp={finishDrawing}
+        onMouseMove={draw}
+        onMouseLeave={finishDrawing} // Para parar de desenhar se o mouse sair do canvas
+        style={{ border: "1px solid black" }}
+      />
+      <div style={{ marginTop: "10px" }}>
+        <button onClick={() => console.log("test")}>teste</button>
+        <label>Escolha a cor do pincel: </label>
+        <input type="color" value={color} onChange={handleColorChange} />
+      </div>
+    </div>
   );
 };
 
