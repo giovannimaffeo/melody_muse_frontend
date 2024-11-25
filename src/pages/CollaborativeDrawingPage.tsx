476c05ff281d0e921f07274e7ffdfcf2ada5ac43
giovanni.maffeo <giovanni.maffeo@gmail.com>
2024-11-25 23:19:46 +0000

    feat: useDrawingContext in collaborativeDrawingPage

--- src/pages/CollaborativeDrawingPage.tsx
+++ src/pages/CollaborativeDrawingPage.tsx
@@ -4,17 +4,34 @@ import DrawingCanvas from '../components/DrawingCanvas';
 import FinishDrawButton from '../components/FinishDrawButton';
 import Timer from '../components/Timer';
 import CollaborativePopup from '../components/CollaborativePopup';
+import { useDrawingContext } from '../context/DrawingContext';
+import { Stroke } from '../interfaces/stroke';
+import { calculateTimeForCollaborative } from '../constants/calculateTimeForCollaborative';
 
 const CollaborativeDrawingPage: React.FC = () => {
+  const { targetStrokes, collaborativeStrokes, setCollaborativeStrokes } = useDrawingContext();
   const [showTimeUp, setShowTimeUp] = useState(false);
   const [finishDrawing, setFinishDrawing] = useState(false);
+  const { hours, minutes, seconds } = calculateTimeForCollaborative(targetStrokes);
+
+  const addCompletedStrokes = (completedStrokes: Stroke[]) => {
+    setCollaborativeStrokes([...collaborativeStrokes, ...completedStrokes]);
+  };
+
+  const removeCompletedStroke = (stroke: Stroke) => {
+    setCollaborativeStrokes(() => collaborativeStrokes.filter((collaborativeStroke) => collaborativeStroke.id !== stroke.id));
+  };
+
+  const removeAllCompletedStrokes = () => {
+    setCollaborativeStrokes([]);
+  };
 
   return (
     <div className='overflow-hidden h-screen w-screen'>
       <Timer 
-        hours={0}
-        minutes={1}
-        seconds={30}
+        hours={hours}
+        minutes={minutes}
+        seconds={seconds}
         onComplete={() => setShowTimeUp(true)}
       />
       {showTimeUp && 
@@ -31,16 +48,36 @@ const CollaborativeDrawingPage: React.FC = () => {
       }
       <div className='grid grid-cols-2 grid-rows-2 h-full w-full'>
         <div className='border border-black'>
-          <DrawingCanvas isFullSize={false} />
+          <DrawingCanvas  
+            addCompletedStrokes={addCompletedStrokes}
+            removeCompletedStroke={removeCompletedStroke}
+            removeAllCompletedStrokes={removeAllCompletedStrokes}
+            isFullSize={false} 
+          />
         </div>
         <div className='border border-black'>
-          <DrawingCanvas isFullSize={false} />
+          <DrawingCanvas  
+            addCompletedStrokes={addCompletedStrokes}
+            removeCompletedStroke={removeCompletedStroke}
+            removeAllCompletedStrokes={removeAllCompletedStrokes}
+            isFullSize={false} 
+          />
         </div>
         <div className='border border-black'>
-          <DrawingCanvas isFullSize={false} />
+          <DrawingCanvas  
+            addCompletedStrokes={addCompletedStrokes}
+            removeCompletedStroke={removeCompletedStroke}
+            removeAllCompletedStrokes={removeAllCompletedStrokes}
+            isFullSize={false} 
+          />
         </div>
         <div className='border border-black'>
-          <DrawingCanvas isFullSize={false} />
+          <DrawingCanvas  
+            addCompletedStrokes={addCompletedStrokes}
+            removeCompletedStroke={removeCompletedStroke}
+            removeAllCompletedStrokes={removeAllCompletedStrokes}
+            isFullSize={false} 
+          />
         </div>
       </div>
       <FinishDrawButton onClick={() => setFinishDrawing(true)} />
